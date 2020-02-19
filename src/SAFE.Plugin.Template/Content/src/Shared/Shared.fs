691fe5d56c1e5ce7be8345320de293bcac9bd1c5
Michał Niegrzybowski <mniegrzybowski@datto.com>
2020-02-19 09:55:13 +0100

    Right now use single project and target concrete part of application based on parameter

--- src/SAFE.Plugin.Template/Content/src/Shared/Shared.fs
+++ src/SAFE.Plugin.Template/Content/src/Shared/Shared.fs
@@ -1,1 +1,21 @@
-namespace Shared
+namespace SAFE.PluginName
+
+open SAFE
+open SAFE.Core
+
+#if (target = "server")
+namespace SAFE.PluginName.Server =
+    type PluginName ()
+        inherit SAFEPlugin()
+        interface ISAFEServerPlugin
+#elif (target = "client")
+    namespace SAFE.PluginName.Client =
+        type PluginName ()
+            inherit SAFEPlugin()
+            interface ISAFEClientPlugin
+#else
+    namespace SAFE.PluginName.Shared =
+        type PluginName ()
+            inherit SAFEPlugin()
+            interface ISAFESharedPlugin
+#endif
